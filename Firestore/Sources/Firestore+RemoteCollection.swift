58006d9e0c6c3e1df4764ca94ae65d875be2ae6d
Mateus Lino <mateusantos05@gmail.com>
2023-07-06 14:08:51 -0300

    Support Firestore in a different file

---
+++ Firestore/Sources/Firestore+RemoteCollection.swift
@@ -0,0 +1,23 @@
+// Created by Mateus Lino
+
+import Firebase
+import FirebaseFirestoreSwift
+import Foundation
+
+extension Firestore: RemoteCollectionDatabase {
+    public func collection(named collectionPath: String) -> RemoteCollection {
+        return collection(collectionPath)
+    }
+}
+
+extension CollectionReference: RemoteCollection {
+    public func documents() async throws -> [RemoteCollectionDocument] {
+        return try await getDocuments().documents
+    }
+}
+
+extension DocumentSnapshot: RemoteCollectionDocument {
+    public func decoded<T: Decodable>(as type: T.Type) throws -> T  {
+        return try data(as: type.self)
+    }
+}
